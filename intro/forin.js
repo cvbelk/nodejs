1a7498952a8ab379062fcf58e25962c8edd2e5de
cvbelk <cvbelk@gmail.com>
2021-02-14 15:50:20 +0200

    forin modified

--- intro/forin.js
+++ intro/forin.js
@@ -3,8 +3,12 @@
 let txt = "";
 const person = {fname:"John", lname:"Doe", age:25}; 
 let x;
+let index = 1;
 for (x in person) {
+  
   txt += x +": " + person[x] + " ";
+  console.log(`${index}: ${x} - ${person[x]}`);
+  index++;
 }
 //document.getElementById("demo").innerHTML = txt;
 console.log(txt);
